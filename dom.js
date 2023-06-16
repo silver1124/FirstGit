7dec822007f1f6f10e0e2b4f8e8e7908680b7905
silver1124 <yashtiwari1124>
2023-06-16 22:17:58 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+// console.dir(document);
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000'
+
+var titles =document.querySelectorAll('.title');
+console.log(titles)
+titles[0].style.color = 'green'
+titles[0].style.fontWeight = 'bold'
