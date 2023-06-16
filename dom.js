01bee236f33fa3cba0285695e193d4ca20e05bf0
silver1124 <yashtiwari1124>
2023-06-16 23:19:42 +0530

    Classname Update

--- dom.js
+++ dom.js
@@ -5,3 +5,12 @@ var titles =document.querySelectorAll('.title');
 console.log(titles)
 titles[0].style.color = 'green'
 titles[0].style.fontWeight = 'bold'
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+items[3].style.backgroundColor='green' 
+items[0].style.fontWeight='bold'
+items[1].style.fontWeight='bold'
+items[2].style.fontWeight='bold'
+items[3].style.fontWeight='bold'
