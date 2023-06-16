d23d4a01b5d9aafe8d767ef0752e2bb56743758c
silver1124 <yashtiwari1124>
2023-06-16 23:53:59 +0530

    new update

--- dom.js
+++ dom.js
@@ -14,3 +14,11 @@ items[0].style.fontWeight='bold'
 items[1].style.fontWeight='bold'
 items[2].style.fontWeight='bold'
 items[3].style.fontWeight='bold'
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+var newItem = document.createElement('li');
+newItem.className = 'list-group-item';
+newItem.textContent = 'Item 5';
+var list = items[0].parentNode;
+list.appendChild(newItem);  
